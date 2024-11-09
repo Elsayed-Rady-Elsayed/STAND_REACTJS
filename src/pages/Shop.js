99cde2b07bc1bf3a0489d1160b0b8c7fb5680eb9
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-09 20:21:32 +0200

    add some sections

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -15,7 +15,7 @@ const Shop = () => {
     dispatch(fetchProducts());
     dispatch(fetchCategories());
     setProductsListFromCate(products);
-  }, [dispatch]);
+  }, []);
 
   const handleChangeListProducts = (category) => {
     if (category === "") {
@@ -28,18 +28,35 @@ const Shop = () => {
     }
   };
 
+  const sortingMethods = {
+    plth: (a, b) => b.price - a.price,
+    phtl: (a, b) => a.price - b.price,
+    aatz: (a, b) => a.title.localeCompare(b.title),
+    azta: (a, b) => b.title.localeCompare(a.title),
+    rev: (a, b) => a.rating.count - b.rating.count,
+  };
+
+  const handleChangeSorting = (ev) => {
+    const s = ev.target.value;
+    const sortFunction = sortingMethods[s];
+
+    setProductsListFromCate((prev) => {
+      return sortFunction ? [...prev].sort(sortFunction) : products;
+    });
+  };
+
   return (
     <div className="text-start md:w-[90%] m-auto p-2 md:p-0 my-[2rem]">
       <p className="md:ps-2 font-bold text-2xl">Shop</p>
       <div className="flex my-5 md:ps-2 justify-between md:flex-row flex-col gap-3">
         <div className="flex items-center gap-3">
           <p>Filter:</p>{" "}
-          <Select placeholder="Sort By">
-            <option value="price low to high">price low to high</option>
-            <option value="price high to low">price high to low</option>
-            <option value="alphapeticaly a to z">alphapeticaly a to z</option>
-            <option value="alphapeticaly z to a">alphapeticaly z to a</option>
-            <option value="reviews">reviews</option>
+          <Select placeholder="Sort By" onChange={handleChangeSorting}>
+            <option value="plth">price low to high</option>
+            <option value="phtl">price high to low</option>
+            <option value="aatz">alphapeticaly a to z</option>
+            <option value="azta">alphapeticaly z to a</option>
+            <option value="rev">reviews</option>
           </Select>
         </div>
         <div>
