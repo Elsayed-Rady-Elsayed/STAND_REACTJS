cf93514316e9292dfc5e8c94ab1087cafe788829
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-09 19:16:58 +0200

    add some sections

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,58 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const SideBar = () => {
+  const { categories, loading, error } = useSelector((state) => state.category);
   return (
-    <div className="text-start p-5 flex flex-col justify-between border-e h-full">
-      <select className="w-full focus:border-0 focus:outline-0 text-lg font-semibold">
-        <option value="" key="" selected disabled className="">
-          Women's Fashion
-        </option>
-        <option value="" key="">
-          jeans
-        </option>
-        <option value="" key="">
-          pluse
-        </option>
-        <option value="" key="">
-          skirt
-        </option>
-      </select>
-      <select className="w-full focus:border-0 focus:outline-0 text-lg font-semibold">
-        <option value="" key="" selected disabled className="">
-          Men's Fashion
-        </option>
-        <option value="" key="">
-          jeans
-        </option>
-        <option value="" key="">
-          T-shirt
-        </option>
-        <option value="" key="">
-          hoodie
-        </option>
-      </select>
-      <Link to={""} className="text-lg font-semibold">
-        Electronics
-      </Link>
-      <Link to={""} className="text-lg font-semibold">
-        Home & Lifestyle
-      </Link>
-      <Link to={""} className="text-lg font-semibold">
-        Medicine
-      </Link>
-      <Link to={""} className="text-lg font-semibold">
-        Sports & Outdoor
-      </Link>
-      <Link to={""} className="text-lg font-semibold">
-        Baby's & Toys
-      </Link>
-      <Link to={""} className="text-lg font-semibold">
-        Groceries & pets
-      </Link>
-      <Link to={""} className="text-lg font-semibold">
-        Health & Beauty
-      </Link>
+    <div className="text-start p-5 flex flex-col justify-start gap-3 border-e h-full">
+      {!error &&
+        !loading &&
+        categories.map((el) => {
+          return (
+            <Link className="text-lg font-semibold capitalize" to={"/ShopAll"}>
+              {el}
+            </Link>
+          );
+        })}
     </div>
   );
 };
